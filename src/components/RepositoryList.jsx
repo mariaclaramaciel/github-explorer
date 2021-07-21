1d02a2c02cb46ff72ec6956441dffc42668f637c
mariaclaramaciel <mariaclaramaciel@icloud.com>
2021-07-21 17:13:54 -0300

    adiciona projeto completo

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+import { useState , useEffect} from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+//https://api.github.com/users/mariaclaramaciel/repos https://api.github.com/orgs/rocketseat/repos
+
+
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/mariaclaramaciel/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+             
+            </ul>
+        </section>
+    );
+}
